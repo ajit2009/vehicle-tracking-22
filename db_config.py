17b16c11fe3c14ff967a8aa0fc67c2becf5406a5
ajit2009 <65666054+ajit2009@users.noreply.github.com>
2025-04-02 17:51:02 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,8 +1,12 @@
 # db_config.py
 
-db_config = {
-    'host': 'sql7.freesqldatabase.com',  # Replace with your free hosting SQL server
-    'user': 'sql7770632',               # Replace with your database username
-    'password': ' rW4FZ1M34e',       # Replace with your database password
-    'database': 'sql7770632'  # Replace with your database name
-}
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host='sql7.freesqldatabase.com',       # Server address
+        user='sql7770632',                     # Your database username
+        password='rW4FZ1M34e',                # Your database password
+        database='sql7770632',                 # Your database name
+        port=3306                             # Default MySQL port
+    )
